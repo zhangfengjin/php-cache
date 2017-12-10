ab2ae1946d2a5813743ab584d015349b57ac8593
zhangfengjin <fengjin1@staff.weibo.com>
2017-12-10 19:07:24 +0800

    缓存门面

--- src/Cache/Facade/Cache.php
+++ src/Cache/Facade/Cache.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * 缓存门面
+ * 继承门面类
  * Created by PhpStorm.
  * User: fengjin1
  * Date: 2017/12/10
@@ -9,7 +11,12 @@
 namespace XYLibrary\Cache\Facade;
 
 
-class Cache
-{
+use XYLibrary\Facade\Facade;
 
+class Cache extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return "cache";
+    }
 }
