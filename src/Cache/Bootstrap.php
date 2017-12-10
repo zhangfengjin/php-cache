33cd82ea0978b81663574bbd06c680c6fb9a260f
zhangfengjin <fengjin1@staff.weibo.com>
2017-12-10 19:27:10 +0800

    缓存启动

--- src/Cache/Bootstrap.php
+++ src/Cache/Bootstrap.php
@@ -11,5 +11,23 @@ namespace XYLibrary\Cache;
 
 class Bootstrap
 {
+    //需要注册到类库中的服务
 
+    protected $bootStraps = [
+        '\XYLibrary\Cache\CacheManagerServiceProvider' => '\XYLibrary\Cache\CacheManagerServiceProvider'
+    ];
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * 启动缓存
+     */
+    public function bootStrap()
+    {
+        //缓存调用方式-启动XYLibrary类库
+        $bootStrap = new \XYLibrary\Bootstrap\Bootstrap();
+        $bootStrap->bootstrap($this->bootStraps);
+    }
 }
