ae3f99e1ad2d64e67c38223a92580843434f120d
zhangfengjin <fengjin1@staff.weibo.com>
2017-12-10 22:28:12 +0800

    配置文件

--- src/Cache/Bootstrap.php
+++ src/Cache/Bootstrap.php
@@ -17,6 +17,7 @@ class Bootstrap
         '\XYLibrary\Cache\CacheManagerServiceProvider' => '\XYLibrary\Cache\CacheManagerServiceProvider'
     ];
 
+    protected $initConfig;
     protected $dirs = [
         'form' => __DIR__ . "/../../Config/",
         'to' => __DIR__ . "/../../../../../Config/"
@@ -24,10 +25,7 @@ class Bootstrap
 
     public function __construct($initConfig = true)
     {
-        if ($initConfig && file_exists($this->dirs["form"])) {
-            //创建缓存基础配置文件
-            copyDir($this->dirs["form"], $this->dirs["to"]);
-        }
+        $this->initConfig = $initConfig;
     }
 
     /**
@@ -37,6 +35,10 @@ class Bootstrap
     {
         //缓存调用方式-启动XYLibrary类库
         $bootStrap = new \XYLibrary\Bootstrap\Bootstrap();
+        if ($this->initConfig && file_exists($this->dirs["form"])) {
+            //创建缓存基础配置文件
+            copyDir($this->dirs["form"], $this->dirs["to"]);
+        }
         $bootStrap->bootstrap($this->bootStraps);
     }
 }
