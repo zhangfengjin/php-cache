43151e06776c7278d90ce4a9b5956583232f4680
zhangfengjin <fengjin1@staff.weibo.com>
2017-12-10 19:06:32 +0800

    连接缓存实现接口

--- src/Cache/Connectors/ConnectInterface.php
+++ src/Cache/Connectors/ConnectInterface.php
@@ -1,5 +1,6 @@
 <?php
 /**
+ * 连接缓存实现接口
  * Created by PhpStorm.
  * User: fengjin1
  * Date: 2017/12/8
@@ -9,7 +10,13 @@
 namespace XYLibrary\Cache\Connectors;
 
 
-interface ConnectorInterface
+interface ConnectInterface
 {
-    function connections(array $config);
+    /**
+     * 连接缓存实现接口
+     * 返回具体的缓存实现对象实例 如RedisStore\FileStore等
+     * @param $config
+     * @return mixed
+     */
+    function connections($config);
 }
